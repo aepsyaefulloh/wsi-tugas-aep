57c89146c9d18715eaf6d5a24d75dc66fa4dfc21
Aep Syaefulloh <aepsyaefulloh0@gmail.com>
2026-04-14 14:49:01 +0700

    Menambahkan Kode

---
+++ percobaan.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost"; // Host MySQL
+$username = "lufiweb_usersmk"; // Username MySQL
+$password = "P4s5Word&"; // Password MySQL
+$database = "lufiweb_smk"; // Nama Database
+?>
